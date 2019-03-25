8d7278481aa221bbffbd05351aeab30d11801c9f
student <melsida.miskaryan@gmail.com>
2019-03-25 13:15:39 +0400

    Some changes

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ int main()
     t.insert(15);
     t.insert(4);
     t.insert(73);
-    t.insert(120);
-    t.insert(20);
+    //t.insert(120);
+    //t.insert(20);
     t.insert(10);
-    t.insert(9);
-    t.insert(13);
+    //t.insert(9);
+    //t.insert(13);
     std::cout << "In-order traverse is   : ";
     t.in_order();
     std::cout << std::endl;
@@ -21,15 +21,19 @@ int main()
     std::cout << "Post-order traverse is : ";
     t.post_order();
     std::cout << std::endl;
-    t.remove(15);
+    t.remove(73);
     std::cout << "In-order traverse is   : ";
     t.in_order();
     std::cout << std::endl;
-    if (t.search(11)) {
+    std::cout << (t.root->right)->right << std::endl;
+    t.remove(15);
+    std::cout << (t.root->right)->data << std::endl;
+    std::cout << (t.root->right)->left << std::endl;
+  /*  if (t.search(11)) {
         std::cout << "Found" << std::endl;
     } else {
         std::cout << "Not found" << std::endl;
     }
     std::cout << "Depth of the tree is: " << t.max_depth() << std::endl;
-    t.level_order();
+    t.level_order(); */
 }
